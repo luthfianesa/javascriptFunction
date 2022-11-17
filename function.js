8c09eefbe3e162e9bf678f7fdbf765ce1cf2e014
luthfianesa <lutvheanesa@gmail.com>
2022-11-17 16:38:36 +0700

    Add semicolon and function average

--- function.js
+++ function.js
@@ -127,41 +127,40 @@ console.log('\n');
 // Counting Discounted Price in January, February and March
 
 function discountedPrice(discountPercentage, month) {
-  let tshirtPrice = 25000
-  let discountPrice = tshirtPrice * discountPercentage / 100
-  let discountedPrice = tshirtPrice - discountPrice
-  return `Discounted Price In ${month} Rp.-${discountedPrice}`
+  let tshirtPrice = 25000;
+  let discountPrice = (tshirtPrice * discountPercentage) / 100;
+  let discountedPrice = tshirtPrice - discountPrice;
+  return `Discounted Price In ${month} Rp.-${discountedPrice}`;
 }
 
 // Price in January
-const countingDiscountedJanuary = discountedPrice(10,'January')
+const countingDiscountedJanuary = discountedPrice(10, "January");
 // Price in February
-const countingDiscountedFebruary = discountedPrice(40,'February')
+const countingDiscountedFebruary = discountedPrice(40, "February");
 // Price in March
-const countingDiscountedMarch = discountedPrice(70,'March')
-
+const countingDiscountedMarch = discountedPrice(70, "March");
 
 console.log(countingDiscountedJanuary);
 console.log(countingDiscountedFebruary);
 console.log(countingDiscountedMarch);
 
-console.log('\n');
+console.log("\n");
 
 // Counting Age
 function countingAge(yearBorn) {
-  let thisYear = new Date().getFullYear()
-  let age = thisYear - yearBorn
-  return `Your age is ${age}` 
+  let thisYear = new Date().getFullYear();
+  let age = thisYear - yearBorn;
+  return `Your age is ${age}`;
 }
 
-const countingMyAge = countingAge(1998)
+const countingMyAge = countingAge(1998);
 console.log(countingMyAge);
 
-console.log('\n');
+console.log("\n");
 
 // // Function Declarative
 // function iniFungsiDeklaratif(parameternya) {
-  
+
 // }
 
 // // Function Expression
@@ -175,7 +174,72 @@ console.log('\n');
 // }
 
 const hitungPangkat = (angka1, angka2) => {
-  return `Hasil dari ${angka1} pangkat ${angka2} adalah ${angka1 ** angka2}`
+  return `Hasil dari ${angka1} pangkat ${angka2} adalah ${angka1 ** angka2}`;
+};
+
+console.log(hitungPangkat(5, 2));
+console.log("\n");
+
+// Function Average
+function countAverage1(value1, value2, value3, value4, value5, value6, value7, value8, value9, value10) {
+  let average = (value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9 + value10) / arguments.length;
+  return average;
 }
 
-console.log(hitungPangkat(5,2));
+console.log(countAverage1(9, 9, 9, 9, 9, 9, 9, 9, 9, 9));
+console.log("\n");
+
+// // Function Average using rest parameter 1
+function countAverage2(...value) {
+  let sum = 0;
+  let totalSum = 0;
+  let result = 0;
+  for (let i = 0; i <= value.length - 1; i++) {
+    sum += value[i];
+    result = sum / arguments.length;
+  }
+  return result;
+}
+
+console.log(countAverage2(90, 90, 90, 90, 90, 90, 90, 90, 90, 90));
+console.log("\n");
+
+// // Function Average using rest parameter 2
+function countAverage2(...value) {
+  let sum = 0;
+  let emptyArray = [];
+  let totalSum = 0;
+  let result = 0;
+  for (let i = 0; i <= value.length - 1; i++) {
+    sum += value[i];
+    // console.log(sum);
+    emptyArray.push(sum);
+    // console.log(emptyArray);
+    // result = sum / countAverage2.length
+  }
+  totalSum = emptyArray.pop();
+  // console.log(totalSum);
+  // console.log(emptyArray);
+  result = totalSum / arguments.length;
+  return result;
+}
+
+console.log(countAverage2(8, 9, 9, 9, 9, 9, 9, 9, 9, 9));
+
+// Function Check Data Type
+function checkDataType(dataType) {
+  return typeof dataType;
+}
+
+// number
+const data1 = checkDataType(5);
+console.log(`This data type is ${data1}`);
+// string
+const data2 = checkDataType("5");
+console.log(`This data type is ${data2}`);
+// boolean
+const data3 = checkDataType(true);
+console.log(`This data type is ${data3}`);
+// array considered as object
+const data4 = checkDataType(["Apex"]);
+console.log(`This data type is ${data4}`);
